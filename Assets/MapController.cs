7b1271ccdad001abd7ec8e6147078a9d3d496487
Xihao He <XHE12@art.edu>
2019-07-31 13:54:07 -0700

    Prepare for Map

--- Assets/MapController.cs
+++ Assets/MapController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillSelector : MonoBehaviour
+public class MapController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
